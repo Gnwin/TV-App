b82200ff5d321e6210cad3f3be1c16414675527b
Godwin Nwachukwu <nwgwin@gmail.com>
2022-04-20 22:29:35 +0100

    fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'TV App',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   devServer: {
